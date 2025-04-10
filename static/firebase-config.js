150390ea8abbb6ce75e617ce19ff41169cf3b137
shubh72010 <108581630+shubh72010@users.noreply.github.com>
2025-04-10 20:09:57 +0530

    Create firebase-config.js

---
+++ static/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD6qceA3bsMVb5fAE--699_omZEQxLCeAM",
+  authDomain: "straitai-v03.firebaseapp.com",
+  projectId: "straitai-v03",
+  storageBucket: "straitai-v03.appspot.com",
+  messagingSenderId: "365452252559",
+  appId: "1:365452252559:web:c83fdf6109666f1c7027fa",
+  measurementId: "G-XYL9Y3QB0W"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
